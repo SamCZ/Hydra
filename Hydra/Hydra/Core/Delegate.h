0c150f65ae021a521d0f11517ed1a70d23771226
Freki <rainb0wdash@seznam.cz>
2019-04-14 14:37:51 +0200

    Added variable delegates

--- Hydra/Hydra/Core/Delegate.h
+++ Hydra/Hydra/Core/Delegate.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Hydra/Core/Log.h"
+#include "Hydra/Core/Container.h"
+
 namespace Hydra
 {
 
@@ -34,5 +37,68 @@ namespace Hydra
 			return std::invoke(_Method, *_Class, params...);
 		}
 	};
+
+
+
+	template<typename ReturnType, typename... ArgsType>
+	struct NammedDelegate
+	{
+		String Name;
+		IDelegate<ReturnType, ArgsType...>* Delegate;
+	};
+
+	template<typename ReturnType, typename... ArgsType>
+	class DelegateEvent
+	{
+	private:
+		Map<String, IDelegate<ReturnType, ArgsType...>*> _Events;
+	public:
+		~DelegateEvent()
+		{
+			for (auto& it : _Events)
+			{
+				delete it.second;
+			}
+		}
+
+		DelegateEvent& operator+=(const NammedDelegate<ReturnType, ArgsType...>& d)
+		{
+			if (_Events.find(d.Name) != _Events.end())
+			{
+				LogError("DelegateEvent +=", d.Name, "Cannot add ! Same event already exists.");
+				delete d.Delegate;
+
+				return *this;
+			}
+
+			_Events[d.Name] = d.Delegate;
+
+			return *this;
+		}
+
+		DelegateEvent& operator-=(const String evtName)
+		{
+			if (_Events.find(evtName) != _Events.end())
+			{
+				delete _Events[evtName];
+
+				_Events.erase(evtName);
+			}
+
+			return *this;
+		}
+
+		void Invoke(ArgsType... args)
+		{
+			for (auto& it : _Events)
+			{
+				it.second->Invoke(args...);
+			}
+		}
+	};
+
+	#define EVENT_NAME(ClassName, MethodName) (String(#ClassName) + ":" + String(#MethodName))
+	#define EVENT(ClassName, MethodName) NammedDelegate<void> { EVENT_NAME(ClassName, MethodName), new Delegate<ClassName, void>(this, &ClassName::MethodName) }
+	#define EVENT_ARGS(ClassName, MethodName, ...) NammedDelegate<void, __VA_ARGS__> { EVENT_NAME(ClassName, MethodName), new Delegate<ClassName, void, __VA_ARGS__>(this, &ClassName::MethodName) }
 
 }
