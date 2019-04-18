0f7a99fd6027898c34c5fd7159464f107c17a3de
Sam <michalconstantine@gmail.com>
2019-04-18 23:32:14 +0200

    Removed test casting

--- Hydra/Hydra/Render/Pipeline/View/MainRenderView.cpp
+++ Hydra/Hydra/Render/Pipeline/View/MainRenderView.cpp
@@ -27,16 +27,14 @@ namespace Hydra
 
 		for (HPrimitiveComponent* cmp : components)
 		{
-			if (cmp->IsA<HStaticMeshComponent>())
-			{
-				HStaticMeshComponent* staticMeshComponent = Cast<HStaticMeshComponent>(cmp);
-
-				//Do something...
-			}
-
 			if (HStaticMeshComponent* staticMeshComponent = cmp->SafeCast<HStaticMeshComponent>())
 			{
-				//Do something...
+				HStaticMesh* mesh = staticMeshComponent->StaticMesh;
+
+				if (mesh)
+				{
+					
+				}
 			}
 		}
 	}
