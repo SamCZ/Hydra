28e65aaf16fa8bcc6d1838ce3f738cb9fa3fe29f
Freki <rainb0wdash@seznam.cz>
2019-04-30 01:22:24 +0200

    Added head tool program

---
+++ HydraHeaderTool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraHeaderTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string generatedHeadFilesFolder = "GeneratedHeaders";
+            string hydraFolder = "Hydra";
+
+            if(!Directory.Exists(generatedHeadFilesFolder))
+            {
+                Directory.CreateDirectory(generatedHeadFilesFolder);
+            }
+
+            foreach (string dir in Directory.GetFiles(hydraFolder, "*", SearchOption.AllDirectories))
+            {
+
+            }
+        }
+    }
+}
