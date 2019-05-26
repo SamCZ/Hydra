e7c7f4c85de5257d596e0267d0b8e6a781a3dd29
Freki <rainb0wdash@seznam.cz>
2019-05-26 13:46:06 +0200

    Implemented viewport.

--- Hydra/Hydra/Render/View/ViewPort.h
+++ Hydra/Hydra/Render/View/ViewPort.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include "Hydra/Core/Delegate.h"
+
 class FViewPort
 {
 private:
+	int _Width;
+	int _Height;
+	bool _IsSizeValid;
 
+	DelegateEvent<void, int, int> _OnResizeEvent;
 public:
 	FViewPort();
+	FViewPort(int width, int height);
+
+	void Resize(int width, int height);
+
+	int GetWidth() const;
+	int GetHeight() const;
+
+	bool IsValid() const;
+
+	void AddResizeListener(const NammedDelegate<void, int, int>& Event);
+	void RemoveListener(const String& EventName);
 };
