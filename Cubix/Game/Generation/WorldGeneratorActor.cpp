3cea5ec13f2692890b5266b4bc0c5065c8f264ff
Sam <rainb0wdash@seznam.cz>
2020-02-26 05:40:48 +0100

    Implemented greedy meshing

--- Cubix/Game/Generation/WorldGeneratorActor.cpp
+++ Cubix/Game/Generation/WorldGeneratorActor.cpp
@@ -125,6 +125,124 @@ void AddFace(ProceduralMesh& mesh, Chunk& chunk, Block& block, float x, float y,
 	mesh.AddQuadIndices();
 }
 
+HStaticMesh* CreateGreedyChunk(WorldPtr world, int chunkX, int chunkZ)
+{
+	ProceduralMesh mesh;
+
+	Vector3i dims = Vector3i(16, 16, 16);
+
+	Chunk* chunk = world->GetChunk(chunkX, chunkZ);
+
+	static float tUnit = 1.0F / (256.0f / 16.0f);
+
+	auto f = [chunk, world, chunkX, chunkZ](int x, int y, int z)
+	{
+		return !IsAir(world->GetBlock(chunkX, chunkZ, x, y, z));
+	};
+
+	auto AddVertex = [&mesh](float x, float y, float z, float u, float v)
+	{
+		mesh.AddVertex(Vector3(x, y, z), Vector2(u, 1.0 - v), Vector3(0.0f, 0.0f, 0.0f), Vector3(255, 0.0f, 0.0f));
+	};
+
+	for (uint8_t d = 0; d < 3; ++d)
+	{
+		int i, j, k, l, w, h;
+
+		int u = (d + 1) % 3;
+		int v = (d + 2) % 3;
+
+		Vector3i x;
+		Vector3i q;
+
+		int mask[16 * 16]; //dims[u] * dims[v]
+
+		q[d] = 1;
+
+		for (x[d] = -1; x[d] < dims[d]; )
+		{
+			//Compute mask
+			int n = 0;
+
+			for (x[v] = 0; x[v] < dims[v]; ++x[v])
+			{
+				for (x[u] = 0; x[u] < dims[u]; ++x[u])
+				{
+					mask[n++] = (0 <= x[d] ? f(x[0], x[1], x[2]) : false) != (x[d] < dims[d] - 1 ? f(x[0] + q[0], x[1] + q[1], x[2] + q[2]) : false);
+				}
+			}
+
+			//Increment x[d]
+			++x[d];
+			//Generate mesh for mask using lexicographic ordering
+
+			n = 0;
+
+			for (j = 0; j < dims[v]; ++j)
+			{
+				for (i = 0; i < dims[u]; )
+				{
+					if (mask[n])
+					{
+						//Compute width
+						for (w = 1; mask[n + w] && i + w < dims[u]; ++w);
+
+						//Compute height (this is slightly awkward
+						bool done = false;
+						for (h = 1; j + h < dims[v]; ++h)
+						{
+							for (k = 0; k < w; ++k)
+							{
+								if (!mask[n + k + h * dims[u]])
+								{
+									done = true;
+									break;
+								}
+							}
+							if (done)
+							{
+								break;
+							}
+						}
+
+						//Add quad
+						x[u] = i;
+						x[v] = j;
+
+						Vector3i du;
+						du[u] = w;
+						Vector3i dv;
+						dv[v] = h;
+
+						AddVertex(x[0], x[1], x[2], 0, 0);
+						AddVertex(x[0] + du[0], x[1] + du[1], x[2] + du[2], 0, tUnit);
+						AddVertex(x[0] + du[0] + dv[0], x[1] + du[1] + dv[1], x[2] + du[2] + dv[2], tUnit, tUnit);
+						AddVertex(x[0] + dv[0], x[1] + dv[1], x[2] + dv[2], tUnit, 0);
+						mesh.AddQuadIndices();
+
+						//Zero-out mask
+						for (l = 0; l < h; ++l)
+							for (k = 0; k < w; ++k)
+							{
+								mask[n + k + l * dims[u]] = false;
+							}
+						//Increment counters and continue
+						i += w;
+						n += w;
+					}
+					else
+					{
+						++i;
+						++n;
+					}
+				}
+			}
+		}
+	}
+
+	return mesh.Create();
+}
+
 HStaticMesh* CreateChunk(WorldPtr world, int chunkX, int chunkZ)
 {
 	LOG("Generating mesh...");
@@ -206,7 +324,7 @@ void WorldGeneratorActor::BeginPlay()
 
 	{
 		HStaticMeshComponent* meshComponent = AddComponent<HStaticMeshComponent>("Mesh");
-		meshComponent->StaticMesh = CreateChunk(gameMode->GetWorld(), 0, 0);
+		meshComponent->StaticMesh = CreateGreedyChunk(gameMode->GetWorld(), 0, 0);
 		staticMaterial.Material->SetTexture("_TerrainTexture", Engine->GetAssetManager()->GetTexture("Assets/Textures/terrain.png"));
 		//staticMaterial.Material->SetSampler("_PixelSampler", Engine->GetGraphics()->CreateSampler("_PixelSampler", WrapMode::WRAP_MODE_BORDER, WrapMode::WRAP_MODE_BORDER, WrapMode::WRAP_MODE_BORDER, false, false, true));
 		meshComponent->StaticMesh->StaticMaterials.push_back(staticMaterial);
